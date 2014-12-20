3e93ab440100fc2dfe8b57bf76b0abc0acd91a81
Cezary Baginski <cezary@chronomantic.net>
2014-12-20 01:42:47 +0100

    Convert specs to RSpec 2.14.7 syntax with Transpec
    This conversion is done by Transpec 2.3.7 with the following command:
        transpec
    
    * 33 conversions
        from: its(:attr) { }
          to: describe '#attr' do subject { super().attr }; it { } end
    
    * 11 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 6 conversions
        from: obj.should
          to: expect(obj).to
    
    * 4 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 2 conversions
        from: == expected
          to: eq(expected)
    
    * 2 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 2 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/guard/migrate_spec.rb
+++ spec/guard/migrate_spec.rb
@@ -21,35 +21,68 @@ RSpec.describe Guard::Migrate do
   describe 'options' do
     context 'bundler' do
       context 'with a gemfile found' do
-        before { File.stub(:exist?).and_return(true) }
-        its(:bundler?) { should be_true }
-        its(:rake_string) { should match(/^bundle exec rake/) }
+        before { allow(File).to receive(:exist?).and_return(true) }
+
+        describe '#bundler?' do
+          subject { super().bundler? }
+          it { should be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/^bundle exec rake/) }
+        end
 
         context 'with bunder set to false' do
           let(:options) { { bundler: false } }
 
-          its(:bundler?) { should be_false }
-          its(:rake_string) { should match(/^rake/) }
+          describe '#bundler?' do
+            subject { super().bundler? }
+            it { should be_false }
+          end
+
+          describe '#rake_string' do
+            subject { super().rake_string }
+            it { should match(/^rake/) }
+          end
         end
       end
       context 'with no gemfile found' do
-        before { File.stub(:exist?).and_return(false) }
-        its(:bundler?) { should_not be_true }
-        its(:rake_string) { should match(/^rake/) }
+        before { allow(File).to receive(:exist?).and_return(false) }
+
+        describe '#bundler?' do
+          subject { super().bundler? }
+          it { should_not be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/^rake/) }
+        end
       end
     end
 
     context 'cmd' do
       context 'without command customization' do
-        its(:cmd?) { should_not be_true }
+        describe '#cmd?' do
+          subject { super().cmd? }
+          it { should_not be_true }
+        end
       end
 
       context 'with command customization' do
-        before { File.stub(:exist?).and_return(false) }
+        before { allow(File).to receive(:exist?).and_return(false) }
         let(:options) { { cmd: 'custom command rake' } }
 
-        its(:cmd?) { should be_true }
-        its(:rake_string) { should match(/^custom command rake/) }
+        describe '#cmd?' do
+          subject { super().cmd? }
+          it { should be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/^custom command rake/) }
+        end
 
         context "without presence of 'rake' keyword" do
           let(:options) { { cmd: 'custom command' } }
@@ -60,9 +93,12 @@ RSpec.describe Guard::Migrate do
         end
 
         context 'with Bundler' do
-          before { File.stub(:exist?).and_return(true) }
+          before { allow(File).to receive(:exist?).and_return(true) }
 
-          its(:rake_string) { should match(/^bundle exec custom command rake/) }
+          describe '#rake_string' do
+            subject { super().rake_string }
+            it { should match(/^bundle exec custom command rake/) }
+          end
         end
 
         context 'with custom rake task specified' do
@@ -71,7 +107,7 @@ RSpec.describe Guard::Migrate do
 
             context 'rake_string' do
               it "should contains 'db:migrate' once" do
-                subject.rake_string.scan('db:migrate').size.should == 1
+                expect(subject.rake_string.scan('db:migrate').size).to eq(1)
               end
             end
           end
@@ -81,7 +117,7 @@ RSpec.describe Guard::Migrate do
 
             context 'rake_string' do
               it "should contains 'db:test:clone' once" do
-                subject.rake_string.scan('db:test:clone').size.should == 1
+                expect(subject.rake_string.scan('db:test:clone').size).to eq(1)
               end
             end
           end
@@ -91,97 +127,150 @@ RSpec.describe Guard::Migrate do
 
     context 'test clone' do
       context 'with no options passed' do
-        its(:test_clone?) { should be_false }
-        its(:rake_string) { should match(/rake db:migrate/) }
-        its(:rake_string) { should_not match(/db:test:clone/) }
+        describe '#test_clone?' do
+          subject { super().test_clone? }
+          it { should be_false }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/rake db:migrate/) }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should_not match(/db:test:clone/) }
+        end
       end
 
       context 'when passed false' do
         let(:options) { { test_clone: false } }
-        its(:test_clone?) { should be_false }
-        its(:rake_string) { should match(/rake db:migrate/) }
-        its(:rake_string) { should_not match(/db:test:clone/) }
+
+        describe '#test_clone?' do
+          subject { super().test_clone? }
+          it { should be_false }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/rake db:migrate/) }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should_not match(/db:test:clone/) }
+        end
       end
 
       context 'when passed true' do
         let(:options) { { test_clone: true } }
-        its(:test_clone?) { should be_true }
-        its(:rake_string) { should match(/rake db:migrate/) }
-        its(:rake_string) { should match(/db:test:clone/) }
+
+        describe '#test_clone?' do
+          subject { super().test_clone? }
+          it { should be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/rake db:migrate/) }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/db:test:clone/) }
+        end
       end
     end
 
     context 'reset' do
       context 'with no options passed' do
-        its(:reset?) { should_not be_true }
+        describe '#reset?' do
+          subject { super().reset? }
+          it { should_not be_true }
+        end
 
         context 'with paths' do
           let(:paths) { ['1234'] }
           it 'rake string should attempt redo of changed migration' do
-            subject.rake_string(paths.first).should match(/rake db:migrate:redo VERSION\=1234/)
+            expect(subject.rake_string(paths.first)).to match(/rake db:migrate:redo VERSION\=1234/)
           end
         end
       end
 
       context 'when passed true' do
         let(:options) { { reset: true } }
-        its(:reset?) { should be_true }
-        its(:rake_string) { should match(/rake db:migrate:reset/) }
+
+        describe '#reset?' do
+          subject { super().reset? }
+          it { should be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/rake db:migrate:reset/) }
+        end
       end
     end
 
     context 'run on start' do
       context 'with no options set' do
-        its(:run_on_start?) { should_not be_true }
+        describe '#run_on_start?' do
+          subject { super().run_on_start? }
+          it { should_not be_true }
+        end
 
         it 'should not run on start' do
-          subject.should_receive(:migrate).never
+          expect(subject).to receive(:migrate).never
           subject.start
         end
 
         it 'should not run migrate on the reload command' do
-          subject.should_receive(:migrate).never
+          expect(subject).to receive(:migrate).never
           subject.reload
         end
 
         it 'should not run migrate on the run all command' do
-          subject.should_receive(:migrate).never
+          expect(subject).to receive(:migrate).never
           subject.run_all
         end
       end
 
       context 'when passed true' do
         let(:options) { { run_on_start: true } }
-        its(:run_on_start?) { should be_true }
+
+        describe '#run_on_start?' do
+          subject { super().run_on_start? }
+          it { should be_true }
+        end
 
         it 'should run migrate on the start' do
-          subject.should_receive(:migrate)
+          expect(subject).to receive(:migrate)
           subject.start
         end
 
         it 'should run migrate on the reload command' do
-          subject.should_receive(:migrate)
+          expect(subject).to receive(:migrate)
           subject.reload
         end
 
         it 'should run migrate on the run all command' do
-          subject.should_receive(:migrate)
+          expect(subject).to receive(:migrate)
           subject.run_all
         end
 
         context 'with reset set to true' do
           let(:options) { { run_on_start: true, reset: true } }
           it 'should run a migrate reset on start' do
-            subject.rake_string.should match(/db:migrate:reset/)
+            expect(subject.rake_string).to match(/db:migrate:reset/)
           end
         end
 
         context 'with reset set to false' do
           let(:options) { { run_on_start: true, reset: false } }
           it 'should run a regular migrate on start' do
-            subject.rake_string.should match(/db:migrate/)
-            subject.rake_string.should_not match(/db:migrate:reset/)
-            subject.rake_string.should_not match(/db:migrate:redo/)
+            expect(subject.rake_string).to match(/db:migrate/)
+            expect(subject.rake_string).not_to match(/db:migrate:reset/)
+            expect(subject.rake_string).not_to match(/db:migrate:redo/)
           end
         end
       end
@@ -189,32 +278,53 @@ RSpec.describe Guard::Migrate do
 
     context 'Rails Environment' do
       context 'when no option is passed' do
-        its(:rails_env) { should be_nil }
+        describe '#rails_env' do
+          subject { super().rails_env }
+          it { should be_nil }
+        end
       end
 
       context 'when a rails environment is passed' do
         let(:options) { { rails_env: 'development' } }
-        its(:rails_env) { should == 'development' }
 
-        its(:rake_string) { should match(/RAILS_ENV=development/) }
+        describe '#rails_env' do
+          subject { super().rails_env }
+          it { should == 'development' }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/RAILS_ENV=development/) }
+        end
       end
     end
 
     context 'Seed the database' do
       context 'when no option is passed' do
-        its(:seed) { should be_nil }
+        describe '#seed' do
+          subject { super().seed }
+          it { should be_nil }
+        end
       end
 
       context 'when set to true' do
         let(:options) { { seed: true } }
-        its(:seed) { should be_true }
-        its(:rake_string) { should match(/db:seed/) }
+
+        describe '#seed' do
+          subject { super().seed }
+          it { should be_true }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/db:seed/) }
+        end
       end
 
       context 'when seed is set to true and clone is set to true' do
         let(:options) { { seed: true, test_clone: true } }
         it 'runs the seed option before the clone option' do
-          subject.rake_string.should match(/db:seed.*db:test:clone/)
+          expect(subject.rake_string).to match(/db:seed.*db:test:clone/)
         end
       end
     end
@@ -222,18 +332,29 @@ RSpec.describe Guard::Migrate do
     context 'when the seeds file is passed as the paths' do
       let(:paths) { ['db/seeds.rb'] }
       let(:options) { { seed: true, test_clone: true } }
-      its(:seed_only_string) { should match(/db:seed db:test:clone/) }
+
+      describe '#seed_only_string' do
+        subject { super().seed_only_string }
+        it { should match(/db:seed db:test:clone/) }
+      end
 
       it 'runs the rake command with seed only' do
-        subject.should_receive(:system).with(subject.seed_only_string)
+        expect(subject).to receive(:system).with(subject.seed_only_string)
         subject.run_on_changes paths
       end
 
       context 'When reset is set to true' do
         let(:options) { { seed: true, reset: true } }
 
-        its(:rake_string) { should match(/db:seed/) }
-        its(:rake_string) { should match(/db:migrate:reset/) }
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/db:seed/) }
+        end
+
+        describe '#rake_string' do
+          subject { super().rake_string }
+          it { should match(/db:migrate:reset/) }
+        end
       end
     end
   end
@@ -242,7 +363,7 @@ RSpec.describe Guard::Migrate do
     # #I don't like this test much - consider refactoring
     let(:paths) { [create_valid_up_and_down_migration('1234_i_like_cheese').path] }
     it 'should run the rake command' do
-      subject.should_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes paths
     end
   end
@@ -251,7 +372,7 @@ RSpec.describe Guard::Migrate do
     let(:paths) { [create_valid_up_and_down_migration('1234_i_like_cheese').path, create_valid_change_migration('1235_i_like_cheese').path] }
     let(:options) { { reset: true, test_clone: true } }
     it 'should run the rake command' do
-      subject.should_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes paths
     end
   end
@@ -261,28 +382,28 @@ RSpec.describe Guard::Migrate do
     it 'should keep valid up/down migrations' do
       migration = create_valid_up_and_down_migration('1234_i_like_cheese')
 
-      subject.should_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes [migration.path]
     end
 
     it 'should keep valid change migrations' do
       migration = create_valid_change_migration('1234_i_like_cheese')
 
-      subject.should_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes [migration.path]
     end
 
     it 'should remove empty up/down migrations' do
       migration = create_invalid_up_and_down_migration('1234_i_like_cheese')
 
-      subject.should_not_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).not_to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes [migration.path]
     end
 
     it 'should remove empty change migrations' do
       migration = create_invalid_change_migration('1234_i_like_cheese')
 
-      subject.should_not_receive(:system).with(subject.rake_string('1234'))
+      expect(subject).not_to receive(:system).with(subject.rake_string('1234'))
       subject.run_on_changes [migration.path]
     end
   end
